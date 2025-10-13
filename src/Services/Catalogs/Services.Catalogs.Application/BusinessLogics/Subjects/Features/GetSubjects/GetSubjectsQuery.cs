d06228b2492282a016c90bd2ed6137ae31d47f4c
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-10-13 15:40:33 +0700

    fix(catalogs): Added subjects controller

---
+++ src/Services/Catalogs/Services.Catalogs.Application/BusinessLogics/Subjects/Features/GetSubjects/GetSubjectsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Services.Catalogs.Application.BusinessLogics.Subjects.DTOs;
+
+namespace Services.Catalogs.Application.BusinessLogics.Subjects.Features.GetSubjects;
+public class GetSubjectsQuery : IRequest<IList<SubjectDto>>
+{
+
+}
