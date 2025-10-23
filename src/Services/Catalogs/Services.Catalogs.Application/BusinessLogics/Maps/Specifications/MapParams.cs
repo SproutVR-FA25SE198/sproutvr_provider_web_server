51b2ccd8b614b8137bb001f7578b9308bb226a54
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-10-23 10:56:20 +0700

    fix(maps): added params and update max page size

--- src/Services/Catalogs/Services.Catalogs.Application/BusinessLogics/Maps/Specifications/MapParams.cs
+++ src/Services/Catalogs/Services.Catalogs.Application/BusinessLogics/Maps/Specifications/MapParams.cs
@@ -4,10 +4,11 @@ namespace Services.Catalogs.Application.BusinessLogics.Maps.Specifications;
 public sealed class MapParams : PagingParams
 {
     public string? Name { get; set; }
-    public Guid? SubjectId { get; set; }
+    public Guid[]? SubjectIds { get; set; } 
     public string? Description { get; set; }
     public decimal? MinPrice { get; set; }
     public decimal? MaxPrice { get; set; }
     public string? MapCode { get; set; }
     public string? Status { get; set; }
+    public string? SortBy { get; set; }
 }
