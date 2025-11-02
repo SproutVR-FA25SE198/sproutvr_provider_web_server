f19dd7ea3f9dd74161df943bd10eed005262edd4
r1nzl3rrr <khanhle9344@gmail.com>
2025-11-02 14:08:03 +0700

    renamed LocationId in ObjectLocation to TaskLocationId, seed changed #53

--- src/Services/Catalogs/Services.Catalogs.Domain/Entities/ObjectLocations/ObjectLocation.cs
+++ src/Services/Catalogs/Services.Catalogs.Domain/Entities/ObjectLocations/ObjectLocation.cs
@@ -7,7 +7,7 @@ namespace Services.Catalogs.Domain.Entities.ObjectLocations;
 public class ObjectLocation : BaseEntity
 {
     public Guid ObjectId { get; set; }
-    public Guid LocationId { get; set; }
+    public Guid TaskLocationId { get; set; }
     public override bool UseIdKey => false;
     // navigation property
     [JsonIgnore]
