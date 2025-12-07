c39a151d49a6a83a7b88f840ab76db0c78f1ca55
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-12-07 13:48:45 +0700

    feat(order): added activation key to order details

--- src/Services/Orders/Services.Orders.Application/BusinessLogics/Orders/Features/GetOrderById/OrderDetailsDto.cs
+++ src/Services/Orders/Services.Orders.Application/BusinessLogics/Orders/Features/GetOrderById/OrderDetailsDto.cs
@@ -13,5 +13,6 @@ public class OrderDetailsDto
     public string RepresentativePhone { get; set; }
     public DateTime CreatedAtUtc { get; set; }
     public DateTime UpdatedAtUtc { get; set; }
+    public string? ActivationKey { get; set; }
     public List<OrderItemDto> OrderItems { get; set; } = [];
 }
